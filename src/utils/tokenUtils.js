20b7b8bba7e23bf21f1fc2115502e4c8ff6262e8
dong279 <dongho0130@daum.net>
2025-05-15 18:56:57 +0900

    logout

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -1,14 +1,12 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
-// 토큰 생성
 const generateToken = (userId) => {
   return jwt.sign({ id: userId }, config.jwtSecret, {
     expiresIn: '24h'
   });
 };
 
-// 토큰 검증
 const verifyToken = (token) => {
   try {
     return jwt.verify(token, config.jwtSecret);
@@ -17,7 +15,6 @@ const verifyToken = (token) => {
   }
 };
 
-// 토큰에서 사용자 ID 추출
 const getUserIdFromToken = (token) => {
   try {
     const decoded = verifyToken(token);
